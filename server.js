eca534b727356752e7fd522022a0acfc4b2d715c
Glitch (xslt) <none>
2019-04-20 02:11:08 +0000

    Get index

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+// XSLT Processing
+app.get('/index', function(req, res) {
+  res.send("Index");
+});
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
