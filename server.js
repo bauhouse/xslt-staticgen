b9971637f85dfd8ba46a2fdf9ebcc67b2a4aa7ab
Stephen Bau <bauhouse@gmail.com>
2019-04-21 13:33:22 -0700

    Set the default port to 3000

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/__build', function(req, res) {
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
