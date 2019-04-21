d9f4e015e76b7a857fd89e70cf1ff18de4f0826b
Stephen Bau <bauhouse@gmail.com>
2019-04-21 13:23:12 -0700

    Remove old routes. Send text on getting the __build route.

--- server.js
+++ server.js
@@ -25,37 +25,8 @@ app.get('/', function(request, response) {
 });
 
 // XSLT processing
-app.get('/index', function(req, res) {
-
-  var xml_file = DATA + '/views/index.xml';
-  var xsl_file = VIEWS + '/index.xsl';
-  
-  var read_xml = new Promise((resolve, reject) => { 
-    fs.readFile(xml_file, (err, data) => {  
-      if (err) throw err;
-      resolve( data.toString() );
-    });
-  });
-
-  var read_xsl = new Promise((resolve, reject) => { 
-    fs.readFile(xsl_file, (err, data) => {  
-      if (err) throw err;
-      resolve( data.toString() );
-    });
-  });
-  
-  Promise.all([read_xml, read_xsl])
-  .then(files => {
-    var xml = files[0];
-    var xsl = files[1];
-    var output = processXSLT(xml, xsl);
-    return res.send(output);
-  })
-  .catch(err => {
-    console.log("XSLT Processing Error");
-    console.log(err);
-  });
-  
+app.get('/__build', function(req, res) {
+  res.send(processXSLT());
 });
 
 // listen for requests :)
@@ -63,14 +34,6 @@ const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-function processXSLT(xml, xsl) {
-
-  var xml_string = xml;
-  var xsl_string = xsl;
-  
-  xml = new DOMParser().parseFromString(xml_string);
-  xsl = new DOMParser().parseFromString(xsl_string);
-
-  return "XSLT Processed" + xsl_string;
-  
+function processXSLT() {
+  return "XSLT Processed"
 }
